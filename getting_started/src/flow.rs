7836f37a7931526700049ce8645c4545579d2c58
Yoshinobu Ogata <92501820+ogty@users.noreply.github.com>
2022-01-27 03:26:02 +0900

    add

---
+++ getting_started/src/flow.rs
@@ -0,0 +1,15 @@
+pub fn flow() {
+    let age = 20;
+
+    if age > 76 {
+        println!("Old age");
+    } else if age > 45 {
+        println!("Middle age");
+    } else if age > 17 {
+        println!("Youth");
+    } else if age > 5 {
+        println!("Boy");
+    } else {
+        println!("Childhood");
+    }
+}
--- getting_started/src/main.rs
+++ getting_started/src/main.rs
@@ -1,10 +1,14 @@
 mod types;
+mod flow;
 mod overload;
 
 fn main() {
     // types
     types::types();
 
+    // flow
+    flow::flow();
+    
     // overload
     overload::abb(10);
     overload::abb(String::from("Feb"));

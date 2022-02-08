e76696f8dcc387fe0ed9ccb1e1f4353d7606fd2f
ogty <mofuiti1803@gmail.com>
2022-02-08 18:15:45 +0900

    add

--- getting_started/src/structure.rs
+++ getting_started/src/structure.rs
@@ -1,3 +1,5 @@
+use std::fmt;
+
 struct Url {
     scheme: String,
     netloc: String,
@@ -9,6 +11,31 @@ struct Url {
 
 struct Position(f32, f32, f32);
 
+struct Password(String);
+
+impl fmt::Display for Password {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "{}", self.0.chars().map(|_| '*').collect::<String>())
+    }
+}
+
+struct MyCalc {
+    data: Vec<i32>
+}
+
+impl MyCalc {
+    fn average(&self) -> f32 {
+        self.data.iter().sum::<i32>() as f32 / self.data.len() as f32
+    }
+}
+
+impl MyCalc {
+    fn median(&mut self) -> i32 {
+        self.data.sort();
+        let mid = self.data.len() / 2;
+        self.data[mid]
+    }
+}
 pub fn main() {
     // "https://example.com/items?page=3&sort=popular"
     let url = Url {
@@ -23,4 +50,17 @@ pub fn main() {
 
     let Position (x, y, z) = Position(-199.036, 75.0000, -2376.8457);
     println!("XYZ: {} / {} / {}", x, y, z);
+
+    let username = "admin";
+    let password = Password("password".to_string());
+    println!("username: {}", username);
+    println!("password: {}", password);
+
+    let array_1 = vec![1, 2, 3, 4, 5];
+    let array_2 = vec![5, 6, 7, 8, 9];
+
+    let instance_1 = MyCalc { data: array_1 };
+    let mut instance_2 = MyCalc { data: array_2 };
+    println!("The average for array_1 is {}", instance_1.average());
+    println!("The median for array_2 is {}", instance_2.median());
 }

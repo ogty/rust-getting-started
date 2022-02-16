572a8698d5218331cf3bded5ddce16001f440835
Yoshinobu Ogata <92501820+ogty@users.noreply.github.com>
2022-02-16 11:56:17 +0900

    Update overload.rs

--- getting_started/src/overload.rs
+++ getting_started/src/overload.rs
@@ -1,7 +1,7 @@
 use std::collections::HashMap;
 
 pub trait Abb<T> {
-    fn abb(&mut self, c:T);
+    fn abb(&mut self, c:T) -> String;
 }
 
 pub struct AbbImpl {
@@ -10,36 +10,36 @@ pub struct AbbImpl {
 }
 
 impl Abb<i32> for AbbImpl {
-    fn abb(&mut self, c:i32) {
+    fn abb(&mut self, c:i32) -> String {
         let months: HashMap<_, _> = self.month_num.iter().zip(self.month_word.iter()).collect();
         let month = months.get(&c);
         match month {
-            Some(v) => println!("{}", v),
-            None => println!("error"),
-        };
+            Some(v) => v.to_string(),
+            None => String::from(""),
+        }
     }
 }
 
-impl Abb<String> for AbbImpl {
-    fn abb(&mut self, c:String) {
+impl Abb<&str> for AbbImpl {
+    fn abb(&mut self, c:&str) -> String {
         let months: HashMap<_, _> = self.month_word.iter().zip(self.month_num.iter()).collect();
-        let month = months.get(&c);
+        let month = months.get(&c.to_string());
         match month {
-            Some(v) => println!("{}", v),
-            None => println!("error"),
-        };
+            Some(v) => v.to_string(),
+            None => String::from(""),
+        }
     }
 }
 
-pub fn abb<T>(data:T) where AbbImpl:Abb<T> {
+pub fn abb<T>(data:T) -> String where AbbImpl:Abb<T> {
     let month_word = vec![
         String::from("Jan"), String::from("Feb"), String::from("Mar"),
         String::from("Apr"), String::from("May"), String::from("Jun"),
         String::from("Jul"), String::from("Aug"), String::from("Sep"),
         String::from("Oct"), String::from("Nov"), String::from("Dec")
     ];
-    let month_num = vec![1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
+    let month_num = (1..13).collect::<Vec<i32>>();
 
     let mut abb = AbbImpl{month_word, month_num};
-    Abb::abb(&mut abb, data);
+    return Abb::abb(&mut abb, data);
 }

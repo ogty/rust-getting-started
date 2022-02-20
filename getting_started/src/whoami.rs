418ccbf8c197ea420480afdcca9bd1ed29c16e1e
ogty <mofuiti1803@gmail.com>
2022-02-21 05:36:12 +0900

    add: annotation

--- getting_started/src/whoami.rs
+++ getting_started/src/whoami.rs
@@ -4,9 +4,9 @@ pub fn whoami() -> Vec<String> {
     let output: Output = Command::new("whoami")
         .output()
         .expect("failed to execute process");
-    let info = String::from_utf8(output.stdout).unwrap();
-    let computer = info.split("\\").collect::<Vec<&str>>()[0];
-    let username = info.split("\\").collect::<Vec<&str>>()[1];
-    let result = vec![computer.to_string(), username.to_string()];
+    let info: String = String::from_utf8(output.stdout).unwrap();
+    let computer: &str = info.split("\\").collect::<Vec<&str>>()[0];
+    let username: &str = info.split("\\").collect::<Vec<&str>>()[1];
+    let result: Vec<String> = vec![computer.to_string(), username.to_string()];
     return result
 }

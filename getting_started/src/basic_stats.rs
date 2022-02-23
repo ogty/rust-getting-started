6719a828641652b708bda5cca63c74c2b0981300
Yoshinobu Ogata <mofuiti1803@gmail.com>
2022-02-24 00:54:45 +0900

    style

--- getting_started/src/basic_stats.rs
+++ getting_started/src/basic_stats.rs
@@ -1,14 +1,17 @@
 use std::collections::HashMap;
 
+
 pub fn average(numbers: &[i32]) -> f32 {
     numbers.iter().sum::<i32>() as f32 / numbers.len() as f32
 }
+
 
 pub fn median(numbers: &mut [i32]) -> i32 {
     numbers.sort();
     let mid: usize = numbers.len() / 2;
     numbers[mid]
 }
+
 
 pub fn mode(numbers: &[i32]) -> i32 {
     let mut occurrences: HashMap<i32, i32> = HashMap::new();
